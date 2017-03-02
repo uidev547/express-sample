f6fafc06e5817bbc00f39eaeb10bcfb080a11805
Kiran <akiran547@gmail.com>
2017-03-02 11:38:02 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,46 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next) {
-  console.log(req.body);
+router.post('/deploy', function(req, res, next) {
+  var GithubWebHook = require('express-github-webhook');
+  var webhookHandler = GithubWebHook({ path: '/', secret: 'hex' });
+  // Now could handle following events
+  webhookHandler.on('*', function (event, repo, data) {
+    console.log('---------start(*)-------');
+    console.log('------------------------');
+    console.log('event', event);
+    console.log('------------------------');
+    console.log('repo', repo);
+    console.log('------------------------');
+    console.log('data', data);
+    console.log('-------end(*)-------');
+  });
+
+  webhookHandler.on('event', function (repo, data) {
+    console.log('----------start(event)------');
+    console.log('repo', repo);
+    console.log('------------------------');
+    console.log('data', data);
+    console.log('-------end(event)----------');
+  });
+
+  webhookHandler.on('reponame', function (event, data) {
+    console.log('----------start(reponame)------');
+    console.log('event', data);
+    console.log('------------------------');
+    console.log('data', data);
+    console.log('-------end(reponame)----------');
+  });
+
+  webhookHandler.on('error', function (err, req, res) {
+    console.log('----------start(error)------');
+    console.log('err', err);
+    console.log('------------------------');
+    console.log('req', req);
+    console.log('------------------------');
+    console.log('res', res);
+    console.log('-------end(error)----------');
+  });
   res.render('index', { title: 'Express_post' });
 });
 
