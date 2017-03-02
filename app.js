11faac5c9cd71e0b181209940890d7280e5f2a6c
Kiran <akiran547@gmail.com>
2017-03-02 12:36:23 +0530

    udpae

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-var GithubWebHook = require('express-github-webhook');
+var GithubWebHook = require('./git-webhook');
 var webhookHandler = GithubWebHook({ path: '/', secret: 'hex' });
 
-app.use('/', index);
-app.use('/users', users);
+webhookHandler.on('success', function (data, req, res) {
+  if( data.payloadData.ref === 'refs/heads/master' ) {
+    res.json({
+      message: 'build _success'
+    });
+  } else {
+    res.json({
+      message: 'no_build'
+    });
+  }
+});
 
+webhookHandler.on('error', function ( data, req, res) {
+  res.statusCode = 400;
+  res.json({
+    message: data.error
+  });
+});
 
 
 // catch 404 and forward to error handler
@@ -42,7 +39,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
